a8cae2149eda1ae8384d9014e20756987bd6d519
GABRIEL CARAMEZ BENVINDO DA SILVA <155910430+GabrielCaramez@users.noreply.github.com>
2025-07-21 15:40:38 -0300

    consertei kkk

--- BellaCrosta/index.php
+++ BellaCrosta/index.php
@@ -14,22 +14,44 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
                 <h1>Home</h1>
                 <li><a href="#">Produtos</a>
                     <ul>
-                        <li><a href="Produto/FormCadastrarProduto.html">Cadastrar</a></li>
+                        <li><a href="Produto/FormCadastroProduto.html">Cadastrar</a></li>
                         <li><a href="Produto/FormConsultarProduto.php">Consultar</a>
                     </ul>
                 </li>
-
                 <li><a href="#">Clientes</a>
                     <ul>
                         <li><a href="Cliente/FormCadastrarCliente.html">Cadastrar</a></li>
                         <li><a href="Cliente/FormConsultarCliente.php">Consultar</a>
                     </ul>
                 </li>
-
-                <li><a href="#">Menu</a>
+                <li><a href="#">Funcionários</a>
                     <ul>
-                        <li><a href="Menu/FormCadastrarMenu.html">Cadastrar</a></li>
-                        <li><a href="Menu/FormConsultarMenu.php">Consultar</a>
+                        <li><a href="Funcionario/FormCadastrarFuncionario.html">Cadastrar</a></li>
+                        <li><a href="Funcionario/FormConsultarFuncionario.php">Consultar</a>
+                    </ul>
+                </li>
+                <li><a href="#">Reserva Individual</a>
+                    <ul>
+                        <li><a href="Reserva_Individual/FormCadastroReserva_Individual.html">Cadastrar</a></li>
+                        <li><a href="Reserva_Individual/FormConsultarReserva_Individual.php">Consultar</a>
+                    </ul>
+                </li>
+                <li><a href="#">Reserva para Eventos</a>
+                    <ul>
+                        <li><a href="Reserva_Eventos/FormCadastroReserva_Eventos.html">Cadastrar</a></li>
+                        <li><a href="Reserva_Eventos/FormConsultarReserva_Eventos.php">Consultar</a>
+                    </ul>
+                </li>
+                <li><a href="#">Estoque</a>
+                    <ul>
+                        <li><a href="Estoque/FormCadastroEstoque.html">Cadastrar</a></li>
+                        <li><a href="Estoque/FormConsultarEstoque.php">Consultar</a>
+                    </ul>
+                </li>
+                <li><a href="#">Pedido</a>
+                    <ul>
+                        <li><a href="Pedido/FormCadastroPedido.html">Cadastrar</a></li>
+                        <li><a href="Pedido/FormConsultarPedido.php">Consultar</a>
                     </ul>
                 </li>
             </ul>
